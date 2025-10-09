82cc9211e13cd0b591534e3d4871139c92a50459
sangram-dotnet-pune <sangramcanva7812@gmail.com>
2025-10-09 12:07:13 +0530

    Dependency Injection for Collection as well as database is added

--- DotnetTask2_WEBAPI1_Collection/Program.cs
+++ DotnetTask2_WEBAPI1_Collection/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddDbContext<EmployeeDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 builder.Services.AddControllers();
+//builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeDbRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
